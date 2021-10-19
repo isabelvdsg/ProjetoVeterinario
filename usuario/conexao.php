10e0cc2f065ab3b95ecc85e52317550b4da0cb8f
isabelvdsg <86683266+isabelvdsg@users.noreply.github.com>
2021-10-19 13:25:44 -0300

    Update conexao.php

--- usuario/conexao.php
+++ usuario/conexao.php
@@ -9,4 +9,5 @@ if (!$connect) die ("<h1>Falha na conexão com o Banco de Dados!</h1>");
 $db = mysqli_select_db($connect, "pweb");
 /*Configurando este arquivo, depois é só você dar um include em suas paginas php,
 isto facilita muito, pois caso haja necessidade de mudar seu Banco de Dados
-você a
+você a altera somente um arquivo*/
+?>
